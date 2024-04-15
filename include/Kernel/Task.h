f26a776443fc43963557eb08f46b29cb087ddeef
Pinwhell <60289470+pinwhell@users.noreply.github.com>
2024-04-15 01:14:35 -0400

    Task Improvements

--- include/Kernel/Task.h
+++ include/Kernel/Task.h
@@ -2,10 +2,13 @@
 
 #include <stdint.h>
 #include <etl/string.h>
+#include <etl/delegate.h>
 
 using Task = uintptr_t;
 
 using TaskComm = etl::string<17>;
 
 Task TaskCurrentGet();
+Task TaskCurrentGetParent();
 TaskComm TaskCommGet(Task task);
+Task TaskByPidGet(size_t vpid);
--- src/Task.cpp
+++ src/Task.cpp
@@ -2,33 +2,25 @@
 #include <Kernel/Syms.h>
 #include <fslc_string.h>
 
-#ifdef BITS64
-__attribute__((always_inline)) uintptr_t __readgsqword(uintptr_t offset) {
-    uintptr_t value;
-    asm volatile(
-        "movq %1, %%rax\n"          // Move the offset to %rdi (first argument register)
-        "movq %%gs:(%%rax), %0"     // Access the value at gs:offset and move it to value
-        : "=r" (value)              // Output: value is written to a register
-        : "r" (offset)              // Input: offset is read from a register
-        : "%rax"                    // Clobbered registers
-        );
-    return value;
-}
+#ifdef BITS32
+#define AX_REG "eax"
+#define TLS_REG "fs"
+#elif BITS64
+#define AX_REG "rax"
+#define TLS_REG "gs"
 #endif
 
-#ifdef BITS32
-__attribute__((always_inline)) uintptr_t __readfsdword(uintptr_t offset) {
+__attribute__((always_inline)) uintptr_t __readtlsword(uintptr_t offset) {
     uintptr_t value;
     asm volatile(
-        "mov %1, %%eax\n"           // Move the offset to %rdi (first argument register)
-        "mov %%fs:(%%eax), %0"      // Access the value at fs:offset and move it to value
+        "movq %1, %%" AX_REG "\n"          // Move the offset to %rdi (first argument register)
+        "movq %%" TLS_REG ":(%%" AX_REG "), %0"     // Access the value at gs:offset and move it to value
         : "=r" (value)              // Output: value is written to a register
         : "r" (offset)              // Input: offset is read from a register
-        : "%eax"                    // Clobbered registers
+        : "%" AX_REG                    // Clobbered registers
         );
     return value;
 }
-#endif
 
 uintptr_t gCurrentTaskOff;
 
@@ -37,12 +29,36 @@ Task TaskCurrentGet()
     if(!gCurrentTaskOff)
         gCurrentTaskOff = uintptr_t(kallsyms_lookup_name("current_task"));
 
-#ifdef BITS64
-    return __readgsqword(gCurrentTaskOff);
-#else
-    return __readfsdword(gCurrentTaskOff);
-#endif
+    return __readtlsword(gCurrentTaskOff);
 }
+
+REGPARAMDECL(Task) find_task_by_vpid(size_t vpid);
+
+Task TaskByPidGet(size_t vpid)
+{
+    if (find_task_by_vpid)
+        return find_task_by_vpid(vpid);
+
+    return 0;
+}
+
+REGPARAMDECL(size_t)  __x64_sys_getppid();
+
+Task TaskCurrentGetParent()
+{
+    if (__x64_sys_getppid)
+    {
+        size_t ppid = __x64_sys_getppid();
+        return TaskByPidGet(ppid);
+    }
+
+    return 0;
+}
+
+//void TaskForEachParent(Task tsk, etl::delegate<bool(Task tsk)> callback)
+//{
+//    for(Task t = tsk;; tsk = )
+//}
 
 REGPARAMDECL(char*) get_task_comm(char* comm, Task tsk);
 REGPARAMDECL(char*) __get_task_comm(char* comm, size_t len, Task tsk);
