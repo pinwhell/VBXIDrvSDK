4633ace4ff1bdb7542ea165ec28b5a1c8840fa6a
Pinwhell <60289470+pinwhell@users.noreply.github.com>
2024-04-15 01:45:03 -0400

    Additions & TBS Testings

--- src/Entry.cxx
+++ src/Entry.cxx
@@ -1,10 +1,14 @@
 #include <stdint.h>
 
+extern bool SDKInit(uintptr_t entry);
 extern bool DriverMain(uintptr_t entry);
 extern void DriverShootdown();
 
 __attribute__((visibility("default"))) extern "C" int _load(uintptr_t entry) 
 {
+    if (int result = SDKInit(entry))
+        return result;
+
     return DriverMain(entry) ? 0 : 1;
 }
 
---
+++ src/Init.cpp
@@ -0,0 +1,43 @@
+#include <cstdint>
+#include <Kernel/Syms.h>
+#include <TBS.hpp>
+
+using namespace TBS;
+
+// SDK Initialization
+
+void TestTBSScan()
+{
+    TBS::UByte buff[] = {
+        0xAA, 0xB1, 0x2C, 0xDD, 0xE2, 0xFF
+    };
+
+    TBS::State<2> state(buff, buff + sizeof(buff));
+
+    auto& desc = state.AddPattern(
+        state.PatternBuilder()
+        .setPattern("AA B? ?C DD E? FF")
+        .setUID("TestPattern")
+        .AddTransformer([](TBS::Pattern::Description& desc, TBS::Pattern::Result result) -> TBS::Pattern::Result {
+            KLOG_PRINT("TBS Middleware Working, Resuilt %llx!!!", result);
+            // Middleware for results found
+            return 0x1;
+            })
+        .Build()
+                ).mDescriptionts[0];
+
+            if (TBS::Scan(state))
+                KLOG_PRINT("TBS Working!!!");
+
+            Pattern::Result result = state["TestPattern"];
+            printk(KERN_INFO "TBS Result %llx!!!", result);
+}
+
+int SDKInit(uintptr_t entry)
+{
+    TestTBSScan();
+
+	KLOG_PRINT("SDKInitialized()");
+
+	return 0;
+}
